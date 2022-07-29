c699023a9ccd7514cd33d62718e417e56d8b7a80
afroditi chintipatoglou <afxintip@hotmail.com>
2022-07-29 23:43:35 +0300

    event_delete, modifications in line through+create

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
+'use strict'
+
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 const ul = document.querySelector("ul");
 let shoppingItems = document.querySelectorAll('li');
+let del = document.querySelectorAll('ul button');
 
 
+//on list items on click change state (done/not done)
+for (let i=0; i< shoppingItems.length; i++){
+	shoppingItems[i].addEventListener('click',function(){
+		shoppingItems[i].classList.toggle('done');
+	});
+	del[i].addEventListener('click',function(){
+		del[i].parentElement.remove();
+	})
+}
+
+//return the length of the input
 function inputLength() {
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
+//create li element based on input and a delete button 
+function createListElement(a) {
+	var li= document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	var btn= document.createElement("button");
+	btn.innerHTML = "delete";
+	ul.appendChild(btn);
+	shoppingItems = document.querySelectorAll('li');
+	del = document.querySelectorAll('ul button');
+	loopForNewList(a);
+	
 }
-
+//on click create an element on the list with argument a created variable for the two matrix (shoppinglist and del)
 function addListAfterClick() {
+	let indexnumber = 0;
 	if (inputLength() > 0) {
-		createListElement();
+		indexnumber = del.length;
+		createListElement(indexnumber);
 	}
 }
 
@@ -27,13 +51,19 @@ function addListAfterKeypress(event) {
 	}
 }
 
-//on list items on click change state (done/not done)
-for (let i=0; i< shoppingItems.length; i++){
-	shoppingItems[i].addEventListener('click',function(){
-		shoppingItems[i].classList.toggle('done');
-	});
-}
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+//on list items on click change state (done/not done)
+function loopForNewList(i) {
+	// for (let i=0; i< shoppingItems.length; i++){
+		shoppingItems[i].addEventListener('click',function(){
+			shoppingItems[i].classList.toggle('done');
+		});
+		del[i].addEventListener('click',function(e){
+			del[i].closest('li').remove();
+		})
+	// }
+}
